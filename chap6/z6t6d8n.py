8f7dcb6a887aa68c5e8dec417c42ca96134cda75
Kinjo Seiga <s21010@std.it-college.ac.jp>
2021-06-03 07:43:06 +0000

    gnrdsj9.py

---
+++ chap6/z6t6d8n.py
@@ -0,0 +1,4 @@
+author = "ウィリアム・フォクナー"
+year_born = "1897"
+
+"{} は {} 年に生まれました。".format(author, year_born)
---
+++ chap6/zqqc2jw
@@ -0,0 +1,8 @@
+author = "Kafka"
+author[0]
+author[1]
+author[2]
+author[3]
+author[4]
+
+
